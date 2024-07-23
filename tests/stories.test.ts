d2d7e720a24436b78f3f0063c19b793218f1e545
niz11 <nizan.goldstein@formlabs.com>
2024-07-23 17:49:31 +0200

    Refactor stories.test file.

--- tests/stories.test.ts
+++ tests/stories.test.ts
@@ -1,21 +1,31 @@
-import axios from 'axios';
-import { getTopStories } from '../src/index';
-import {ParamsObjectType} from '../src/thenewsapiTypes';
+import axios from "axios";
+import { getTopStories } from "../src/index";
+import { ParamsObjectType } from "../src/thenewsapiTypes";
 
-jest.mock('axios');
+jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-test('fetches top stories correctly', async () => {
-  const apiToken = 'your-api-token';
-  const locale = "ca";
-  const limit = "5";
-  const responseData = [{ title: 'Story 1' }, { title: 'Story 2' }, { title: 'Story 3' }];
-  const expectedUrl = `https://api.thenewsapi.com/v1/news/top?api_token=${apiToken}&locale=${locale}&limit=${limit}`;
+describe("getTopStories", () => {
+  it("fetches top stories correctly with locale and limit", async () => {
+    const apiToken = "your-api-token";
+    const locale = "ca";
+    const limit = "5";
+    const responseData = [
+      { title: "Story 1" },
+      { title: "Story 2" },
+      { title: "Story 3" },
+    ];
+    const expectedUrl = `https://api.thenewsapi.com/v1/news/top?api_token=${apiToken}&locale=${locale}&limit=${limit}`;
 
-  mockedAxios.get.mockResolvedValueOnce({ data: responseData });
-  const paramsObject: ParamsObjectType = {api_token: apiToken, locale: locale, limit: limit};
-  const result = await getTopStories(paramsObject);
+    mockedAxios.get.mockResolvedValueOnce({ data: responseData });
+    const paramsObject: ParamsObjectType = {
+      api_token: apiToken,
+      locale: locale,
+      limit: limit,
+    };
+    const result = await getTopStories(paramsObject);
 
-  expect(axios.get).toHaveBeenCalledWith(expectedUrl);
-  expect(result).toEqual(responseData);
+    expect(axios.get).toHaveBeenCalledWith(expectedUrl);
+    expect(result).toEqual(responseData);
+  });
 });
