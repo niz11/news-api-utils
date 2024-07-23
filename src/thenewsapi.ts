9fbcaba9f02ae1c6d328aea1279e5644501d005f
niz11 <nizan.goldstein@formlabs.com>
2024-07-23 17:46:28 +0200

    Add all params to get top stories function.

--- src/thenewsapi.ts
+++ src/thenewsapi.ts
@@ -1,5 +1,5 @@
-const axios = require('axios');
-
+import axios from 'axios';
+import { ParamsObjectType } from './thenewsapiTypes';
 /**
  * Retrieves the top news stories using thenewsapi API.
  * 
@@ -12,8 +12,34 @@ const axios = require('axios');
 
 const baseUrl = 'https://api.thenewsapi.com/v1/news/';
 
-const getTopStories = async (apiToken: string, locale = 'us', limit = 3): Promise<any> => { 
-    const params = new URLSearchParams({api_token: apiToken, locale: locale, limit: limit.toString()});
+const getTopStoriesParamsHelp = () => {
+    return {
+        apiToken: 'Your API token which can be found on your account dashboard.',
+        search: 'Use the search as a basic search tool by entering regular search terms or it has more advanced usage to build search queries: + signifies AND operation, | signifies OR operation, - negates a single token, \" wraps a number of tokens to signify a phrase for searching. * at the end of a term signifies a prefix query, ( and ) signify precedence. When using special characters (+, -, |, ", *, ()) you MUST URL-encode this parameter.',
+        search_fields: "Comma separated list of fields to apply the search parameter to. Supported fields: title | description | keywords | main_text. E.g. search_fields=title,description",
+        locale: 'Comma separated list of country codes to include in the result set. Default is all countries.',
+        categories: 'Comma separated list of category codes to include in the result set. Default is all categories. Supported categories: general | science | sports | business | health | entertainment | tech | politics | food | trave. E.g. categories=general,science',
+        exclude_categories: 'Comma separated list of category codes to exclude from the result set. Default is no categories excluded. Supported categories: general | science | sports | business | health | entertainment | tech | politics | food | trave. E.g. exclude_categories=general,science',
+        domains: "Comma separated list of domains to include. List of domains can be obtained through our Sources endpoint, found further down this page.",
+        exclude_domains: "Comma separated list of domains to exclude. List of domains can be obtained through our Sources endpoint, found further down this page.",
+        source_ids: "Comma separated list of source IDs to include. List of source IDs can be obtained through our Sources endpoint, found further down this page.",
+        exclude_source_ids: "Comma separated list of source IDs to exclude. List of source IDs can be obtained through our Sources endpoint, found further down this page.",
+        language: "Comma separated list of language codes to include in the result set. Default is all languages.",
+        published_before: "Date in the format:  Y-m-d\TH:i:s | Y-m-d\TH:i | Y-m-d\TH | Y-m-d | Y-m | Y.",
+        published_on: "Date in the format:  Y-m-d",
+        sort: "Sorted by score if search is used, otherwise by published_on. Default is published_at unless search is used and sorting by published_at is not included, in which case relevance_score is used.",
+        limit: 'Specify the number of articles you want to return in the request. The maximum limit is based on your plan.',
+        page: 'Specify the page number of the results you want to return. Default is 1.',
+    }
+}
+const getTopStories = async (options: ParamsObjectType): Promise<any> => { 
+    const filteredParamsObject = Object.keys(options).reduce((acc, key) => {
+        if (options[key] !== undefined) {
+            acc[key] = options[key];
+        }
+        return acc;
+    }, {} as { [key: string]: any });
+    const params = new URLSearchParams(filteredParamsObject);
     const endpoint = `${baseUrl}top`;
     const url = `${endpoint}?${params}`;
     try {  
@@ -38,4 +64,4 @@ const getAllNews = async (apiToken: string, language = 'en', limit = 3): Promise
     }
 }
 
-export {getTopStories, getAllNews};
+export {getTopStoriesParamsHelp, getTopStories, getAllNews};
--- tests/stories.test.ts
+++ tests/stories.test.ts
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import { getTopStories } from '../src/index';
+import {ParamsObjectType} from '../src/thenewsapiTypes';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 test('fetches top stories correctly', async () => {
   const apiToken = 'your-api-token';
-  const locale = 'ca';
-  const limit = 5;
+  const locale = "ca";
+  const limit = "5";
   const responseData = [{ title: 'Story 1' }, { title: 'Story 2' }, { title: 'Story 3' }];
   const expectedUrl = `https://api.thenewsapi.com/v1/news/top?api_token=${apiToken}&locale=${locale}&limit=${limit}`;
 
   mockedAxios.get.mockResolvedValueOnce({ data: responseData });
-
-  const result = await getTopStories(apiToken, locale, limit);
+  const paramsObject: ParamsObjectType = {api_token: apiToken, locale: locale, limit: limit};
+  const result = await getTopStories(paramsObject);
 
   expect(axios.get).toHaveBeenCalledWith(expectedUrl);
   expect(result).toEqual(responseData);
