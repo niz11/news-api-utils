868288457a8156954854159d0323a021d4e196dc
niz11 <nizan.goldstein@formlabs.com>
2024-07-24 16:35:53 +0200

    Fix getAllNews function.

--- tests/newsapi.test.ts
+++ tests/newsapi.test.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
-import { getTopStories } from "../src/index";
+import { getTopStories, getAllNews } from "../src/index";
 import { ParamsObjectType } from "../src/thenewsapiTypes";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+const apiToken = "your-api-token";
+const locale = "ca";
+const limit = "5";
+const responseData = [
+  { title: "Story 1" },
+  { title: "Story 2" },
+  { title: "Story 3" },
+];
 
 describe("getTopStories", () => {
-  const apiToken = "your-api-token";
-  const locale = "ca";
-  const limit = "5";
-  const responseData = [
-    { title: "Story 1" },
-    { title: "Story 2" },
-    { title: "Story 3" },
-  ];
   beforeEach(() => {
     mockedAxios.get.mockReset();
   });
-  
+
   const baseUrl = "https://api.thenewsapi.com/v1/news/top?";
   it("fetches top stories correctly with locale and limit", async () => {
     const expectedUrl = `${baseUrl}api_token=${apiToken}&locale=${locale}&limit=${limit}`;
@@ -68,16 +68,33 @@ describe("getTopStories", () => {
   });
 
   it("build all above combined", async () => {
-  const expectedUrl = `${baseUrl}api_token=${apiToken}&search=usd+%2B+gbp+-cad&categories=science%2Csports&published_after=2024-07-23T10%3A00%3A00`;
-  mockedAxios.get.mockResolvedValueOnce({ data: responseData });
-  const paramsObject: ParamsObjectType = {
-    api_token: apiToken,
-    search: "usd + gbp -cad",
-    categories: "science,sports",
-    published_after: "2024-07-23T10:00:00",
-  };
-  const result = await getTopStories(paramsObject);
-  expect(axios.get).toHaveBeenCalledWith(expectedUrl);
-  expect(result).toEqual(responseData);
+    const expectedUrl = `${baseUrl}api_token=${apiToken}&search=usd+%2B+gbp+-cad&categories=science%2Csports&published_after=2024-07-23T10%3A00%3A00`;
+    mockedAxios.get.mockResolvedValueOnce({ data: responseData });
+    const paramsObject: ParamsObjectType = {
+      api_token: apiToken,
+      search: "usd + gbp -cad",
+      categories: "science,sports",
+      published_after: "2024-07-23T10:00:00",
+    };
+    const result = await getTopStories(paramsObject);
+    expect(axios.get).toHaveBeenCalledWith(expectedUrl);
+    expect(result).toEqual(responseData);
+  });
+});
+
+describe("getAllNews", () => {
+  it("fetches all news correctly with some params", async () => {
+    const baseUrl = "https://api.thenewsapi.com/v1/news/all?";
+    const expectedUrl = `${baseUrl}api_token=${apiToken}&search=usd+%2B+gbp+-cad&categories=science%2Csports&published_after=2024-07-23T10%3A00%3A00`;
+    mockedAxios.get.mockResolvedValueOnce({ data: responseData });
+    const paramsObject: ParamsObjectType = {
+      api_token: apiToken,
+      search: "usd + gbp -cad",
+      categories: "science,sports",
+      published_after: "2024-07-23T10:00:00",
+    };
+    const result = await getAllNews(paramsObject);
+    expect(axios.get).toHaveBeenCalledWith(expectedUrl);
+    expect(result).toEqual(responseData);
   });
 });
