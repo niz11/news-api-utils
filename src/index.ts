6c92ccec33eea43972549c64099a789b26f3276e
niz11 <nizan.goldstein@formlabs.com>
2024-07-29 16:35:26 +0200

    Refactor project to have multiple apis structure.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import {getTopStoriesParamsHelp, getTopStories, getAllNews, getSimilarStoriesByUUID} from './thenewsapi';
-import {ParamsObjectType} from './thenewsapiTypes';
-
-export {ParamsObjectType, getTopStoriesParamsHelp, getTopStories, getAllNews, getSimilarStoriesByUUID};
-  
-
-
+export * as thenewsapi from './thenewsapi';
---
+++ src/thenewsapi/index.ts
@@ -0,0 +1,5 @@
+export * from './thenewsapi';
+export * from './thenewsapiTypes';
+  
+
+
